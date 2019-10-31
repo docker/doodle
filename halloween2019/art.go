a868b932eb4edb4221ccc237c7d3973b6e070107
Patrick Devine <pdevine@sonic.net>
2019-10-30 19:48:42 -0700

    Happy Halloween!

---
+++ halloween2019/art.go
@@ -0,0 +1,193 @@
+package main
+
+const moon_p0 = `
+xxxx_..._
+xx.:::::::.
+x:::::::::::
+x:::::::::::
+x`+"`"+`:::::::::'
+xxx`+"`"+`':::''`
+
+const moon_p1 = `
+xxxx_..._
+xx.::::. `+"`"+`.
+x:::::::.  :
+x::::::::  :
+x`+"`"+`::::::' .'
+xxx`+"`"+`'::'-'`
+
+const moon_p2 = `
+xxxx_..._
+xx.::::  `+"`"+`.
+x::::::    :
+x::::::    :
+x`+"`"+`:::::   .'
+xxx`+"`"+`'::.-'`
+
+const moon_p3 = `
+xxxx_..._
+xx.::'   `+"`"+`.
+x:::       :
+x:::       :
+x`+"`"+`::.     .'
+xxx`+"`"+`':..-'`
+
+const moon_p4 = `
+xxxx_..._
+xx.'     `+"`"+`.
+x:         :
+x:         :
+x`+"`"+`.       .'
+xxx`+"`"+`-...-'`
+
+const moon_p5 = `
+xxxx_..._
+xx.'   `+"`"+`::.
+x:       :::
+x:       :::
+x`+"`"+`.     .::'
+xxx`+"`"+`-..:''`
+
+const moon_p6 = `
+xxxx_..._
+xx.'  ::::.
+x:    ::::::
+x:    ::::::
+x`+"`"+`.   :::::'
+xxx`+"`"+`-.::''`
+
+const moon_p7 = `
+xxxx_..._
+xx.' .::::.
+x:  ::::::::
+x:  ::::::::
+x`+"`"+`. '::::::'
+xxx`+"`"+`-.::''`
+
+const moon_p8 = `
+xxxx_..._
+xx.:::::::.
+x:::::::::::
+x:::::::::::
+x`+"`"+`:::::::::'
+xxx`+"`"+`':::''`
+
+const happy_halloween_c0 = `
+@@@xx@@@xxx@@@@@@xxx@@@@@@@xxx@@@@@@@xxx@@@x@@@xxxxx@@@xx@@@xxx@@@@@@xxx@@@xxxxxxx@@@xxxxxxxx@@@@@@xxx@@@xx@@@xx@@@xx@@@@@@@@xx@@@@@@@@xx@@@xx@@@  
+@@@xx@@@xx@@@@@@@@xx@@@@@@@@xx@@@@@@@@xx@@@x@@@xxxxx@@@xx@@@xx@@@@@@@@xx@@@xxxxxxx@@@xxxxxxx@@@@@@@@xx@@@xx@@@xx@@@xx@@@@@@@@xx@@@@@@@@xx@@@@x@@@  
+@@!xx@@@xx@@!xx@@@xx@@!xx@@@xx@@!xx@@@xx@@!x!@@xxxxx@@!xx@@@xx@@!xx@@@xx@@!xxxxxxx@@!xxxxxxx@@!xx@@@xx@@!xx@@!xx@@!xx@@!xxxxxxx@@!xxxxxxx@@!@!@@@  
+!@!xx@!@xx!@!xx@!@xx!@!xx@!@xx!@!xx@!@xx!@!x@!!xxxxx!@!xx@!@xx!@!xx@!@xx!@!xxxxxxx!@!xxxxxxx!@!xx@!@xx!@!xx!@!xx!@!xx!@!xxxxxxx!@!xxxxxxx!@!!@!@!  
+@!@!@!@!xx@!@!@!@!xx@!@@!@!xxx@!@@!@!xxxx!@!@!xxxxxx@!@!@!@!xx@!@!@!@!xx@!!xxxxxxx@!!xxxxxxx@!@xx!@!xx@!!xx!!@xx@!@xx@!!!:!xxxx@!!!:!xxxx@!@x!!@!  
+!!!@!!!!xx!!!@!!!!xx!!@!!!xxxx!!@!!!xxxxxx@!!!xxxxxx!!!@!!!!xx!!!@!!!!xx!!!xxxxxxx!!!xxxxxxx!@!xx!!!xx!@!xx!!!xx!@!xx!!!!!:xxxx!!!!!:xxxx!@!xx!!!  
+!!:xx!!!xx!!:xx!!!xx!!:xxxxxxx!!:xxxxxxxxx!!:xxxxxxx!!:xx!!!xx!!:xx!!!xx!!:xxxxxxx!!:xxxxxxx!!:xx!!!xx!!:xx!!:xx!!:xx!!:xxxxxxx!!:xxxxxxx!!:xx!!!  
+:!:xx!:!xx:!:xx!:!xx:!:xxxxxxx:!:xxxxxxxxx:!:xxxxxxx:!:xx!:!xx:!:xx!:!xxx:!:xxxxxxx:!:xxxxxx:!:xx!:!xx:!:xx:!:xx:!:xx:!:xxxxxxx:!:xxxxxxx:!:xx!:!  
+::xxx:::xx::xxx:::xxx::xxxxxxxx::xxxxxxxxxx::xxxxxxx::xxx:::xx::xxx:::xxx:: ::::xxx:: ::::xx:::::x::xxx::::x::x:::xxxx::x::::xxx::x::::xxx::xxx::  
+x:xxx:x:xxx:xxx:x:xxx:xxxxxxxxx:xxxxxxxxxxx:xxxxxxxxx:xxx:x:xxx:xxx:x:xx:x::x:x:xx:x::x:x:xxx:x:xx:xxxxx::x:xx:x:xxxx:x::x::xxx:x::x::xxx::xxxx:
+`
+
+const whale_c0 = `xxxxxxxxxxxxxxx##xxxxxxxx.xxxxx
+xxxxxxxxx##x##x##xxxxxxx==xxxxx
+xxxxxx##x##x##x##xxxxxx===xxxxx
+xx/""""""""""""""""\___/x===xxx
+x{                      /xx===x
+xx\______ X          __/xxxxxxx
+xxxx\    \        __/xxxxxxxxxx
+xxxxx\____\______/xxxxxxxxxxxxx`
+
+const whale_c0_rev = `xxxxx.xxxxxxxx##xxxxxxxxxxxxxxx
+xxxxx==xxxxxxx##x##x##xxxxxxxxx
+xxxxx===xxxxxx##x##x##x##xxxxxx
+xxx===x\___/""""""""""""""""\xx
+x===xx\                      }x
+xxxxxxx\__          X ______/xx
+xxxxxxxxxx\__        /    /xxxx
+xxxxxxxxxxxxx\______/____/xxxxx`
+
+
+const cloud_c0 = `
+xxxxxxxxx_ _ __.
+xxxxxxx('       ).
+xxxxxx(          ).
+xxxxx_(          '''.
+x.=('(           .   )
+((        (..____.:'-'
+'(        )_),
+xx' _______.:'   )
+xxxxxxxxxx-----'`
+
+const cloud_c1 = `
+xxxx.--
+x.+(   )
+x(   .  )
+(   (   ))
+x'- __.'`
+
+const coffin_top_c0 = `
+xxxxx,________________________________________,`
+
+const coffin_c0 = `
+xxx,-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx-,
+xx+==============================================+
+xx|                                              |
+x(|      =====          =====         =====      |)
+xx|                                              |
+xx|______________________________________________|
+xxx`+"`"+`| |""""""""""""""""""""""""""""""""""""""| |'
+xxxx| |""""""""""""""""""""""""""""""""""""""| |
+xxxx| |""""""""""""""""""""""""""""""""""""""| |
+xxx==============================================`
+
+
+const bigFlame1 = `
+xx.
+x.,
+xooO,
+xoOOoO
+OoOOooO`
+
+const bigFlame2 = `
+xx,
+xxx,+
+xx,Oo.
+x,ooOo
+oO+oOO+`
+
+const bigFlame3 = `
+xxx.
+xx.,
+xx.oO,
+x+OO,O
+OoOOoOO`
+
+
+const torch = `
+ .=======,
+(  -.  _  )
+ `+"`"+`._____,'
+   |   |
+   |   |
+   | , |
+   |   |
+   |   |
+   | | |
+   |   |
+   |.  |
+  =======`
+ 
+
+const ghost_c0 = `
+x.-.
+(o o)
+| O \
+x\   \
+xx`+"`"+`~~~'
+`
+
+const ghost_c1 = `
+xxx.-.
+xx(o o)
+xx/ O |
+x/   /
+`+"`"+`~~~'
+`
+
